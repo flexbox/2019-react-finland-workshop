312f06a29ddfb39c972fc606d50c24d1ff847cd0
David Leuliette <dleuliette@gmail.com>
2019-04-24 16:47:54 +0300

    Add code splitting

--- 6-suspense/app/src/index.js
+++ 6-suspense/app/src/index.js
@@ -1,19 +1,17 @@
-import "normalize.css/normalize.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo-hooks";
+import 'normalize.css/normalize.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo-hooks';
 
 const client = new ApolloClient({
-  uri: "https://www.graphqlhub.com/graphql"
+  uri: 'https://www.graphqlhub.com/graphql'
 });
 
 ReactDOM.render(
-  <React.unstable_ConcurrentMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.unstable_ConcurrentMode>,
-  document.getElementById("root")
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
 );
--- 7-code-splitting/app/src/App.js
+++ 7-code-splitting/app/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-import Profile from "./Profile";
-import Repositories from "./Repositories";
-const Footer = React.lazy(() => import("./Footer"));
+import React from 'react';
+import Profile from './Profile';
+import Repositories from './Repositories';
+
+// Normal import
+// import Chart from './Chart';
+
+// Lazy loading
+const Footer = React.lazy(() => import('./Footer'));
+const Chart = React.lazy(() => import('./Chart'));
 
 function App() {
   return (
     <div>
-      <Profile />
-      <Repositories />
-      <React.Suspense fallback={<div>Loading the footer …</div>}>
-        <Footer />
-      </React.Suspense>
+      <div style={{ float: 'left' }}>
+        <Profile />
+        <Repositories />
+      </div>
+      <div style={{ float: 'right' }}>
+        <React.Suspense fallback={<div>Loading the Chart …</div>}>
+          <Chart />
+        </React.Suspense>
+        <React.Suspense fallback={<div>Loading the footer …</div>}>
+          <Footer />
+        </React.Suspense>
+      </div>
     </div>
   );
 }
