543a040c37c1a97171fa3f8ddea8e93b86b1d087
David Leuliette <dleuliette@gmail.com>
2019-04-24 11:11:14 +0300

    Add map with useEffect and useRef

--- 1-useEffect-and-useRef/app/src/Map.js
+++ 1-useEffect-and-useRef/app/src/Map.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from 'react';
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function Map() {
-  return <div style={{ width: 600, height: 400, backgroundColor: "#ddd" }} />;
+  const element = useRef(null);
+  console.log('TCL: Map -> element', element);
+
+  useEffect(() => {
+    console.log('run effect');
+    console.log('TCL: Map -> element', element.current);
+    const map = Leaflet.map(element.current).setView([51.505, -0.09], 13);
+
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }, []);
+
+  return (
+    <div
+      ref={element}
+      style={{ width: 600, height: 400, backgroundColor: '#ddd' }}
+    />
+  );
 }
 
 export default Map;
