e0d409152c4da868be5cb1b0837ba92bd347c4f7
David Leuliette <dleuliette@gmail.com>
2019-04-24 13:22:59 +0300

    add useLayoutEffect for textarea

--- 3-useLayoutEffect/app/src/TextArea.js
+++ 3-useLayoutEffect/app/src/TextArea.js
@@ -1,9 +1,20 @@
-import React from "react";
-// import calculateNodeHeight from "./calculateNodeHeight";
+import React, { useRef, useLayoutEffect } from 'react';
+import calculateNodeHeight from './calculateNodeHeight';
 
 function TextArea(props) {
+  const textareaElement = useRef(null);
+
+  useLayoutEffect(() => {
+    console.log('run effect');
+    console.log('TCL: TextArea -> textareaElement', textareaElement);
+    const height = calculateNodeHeight(textareaElement.current);
+    console.log('TCL: TextArea -> height', height);
+    textareaElement.current.style.height = `${height}px`;
+  });
+
   return (
     <textarea
+      ref={textareaElement}
       value={props.value}
       onChange={event => {
         props.onChange(event.target.value);
